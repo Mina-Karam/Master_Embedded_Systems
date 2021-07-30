a5c383f74dc677c5b78ad589c57facb25b95684d
Mina Karam <38835380+Mina-Karam@users.noreply.github.com>
2021-07-31 00:07:02 +0200

    adding Unit_2 >> Assignment_1 >> EX5 C Program to Find ASCII Value of a Character

---
+++ Unit_2_C_Programming/Assignment_1_C_Basics/EX4_C_Program_to_Multiply_Two_Floating_Point_Numbers.c
@@ -0,0 +1,25 @@
+/*
+ ======================================================================================================================
+ Name        : EX4_C_Program_to_Multiply_Two_Floating_Point_Numbers.c
+ Author      : Mina Karam
+ Created on	 : Jul 30, 2021
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX4 C Program to Multiply two Floating Point
+ 	 	 	   Numbers using Ansi-style (c99)
+ ======================================================================================================================
+ */
+//#include <stdio.h>
+
+//int main( )
+//{
+//    float num1, num2, multi;
+//
+//    printf("Enter two numbers: ");
+//    scanf("%f %f",&num1,&num2);
+//
+//    multi = num1 * num2;
+//
+//    printf("multi: %f",multi);
+//
+//    return 0;
+//}
+
---
+++ Unit_2_C_Programming/Assignment_1_C_Basics/EX5_C_Program_to_Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,24 @@
+/*
+ ======================================================================================================================
+ Name        : EX5_C_Program_to_Find_ASCII_Value_of_a_Character.c
+ Author      : Mina Karam
+ Created on	 : Jul 30, 2021
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX5 C Program to Find ASCII Value of a Character
+ 	 	 	   using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+//#include <stdio.h>
+
+//int main()
+//{
+//    char c;
+//
+//    printf("Enter a character: ");
+//    scanf("%c",&c);
+//
+//    printf("ASCII value of %c = %d",c,c);
+//
+//    return 0;
+//}
+
