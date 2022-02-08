5491c28f285024d4342140f47a1981b61ecb9c43
Mina Karam <mina.karam799@gmail.com>
2022-02-08 21:28:59 +0200

    Update LCD Driver
    - Change commands macro name
    - Change LCD_CLEAR_SCREEN function name
    - Create LCD_GOTOXY function

--- Unit_7_MCU_Essential_Peripherals/Lesson_2_GPIO_Part1/LCD_driver/lcd.h
+++ Unit_7_MCU_Essential_Peripherals/Lesson_2_GPIO_Part1/LCD_driver/lcd.h
@@ -27,30 +27,32 @@
 #define EIGHT_BIT_MODE
 //#define FOUR_BIT_MODE
 
-#define LCD_FUNCTION_8BIT_2LINES   					(0x38)
-#define LCD_FUNCTION_4BIT_2LINES   					(0x28)
-#define LCD_MOVE_DISP_RIGHT       					(0x1C)
-#define LCD_MOVE_DISP_LEFT   						(0x18)
-#define LCD_MOVE_CURSOR_RIGHT   					(0x14)
-#define LCD_MOVE_CURSOR_LEFT 	  					(0x10)
-#define LCD_DISP_OFF   								(0x08)
-#define LCD_DISP_ON_CURSOR   						(0x0E)
-#define LCD_DISP_ON_CURSOR_BLINK   					(0x0F)
-#define LCD_DISP_ON_BLINK   						(0x0D)
-#define LCD_DISP_ON   								(0x0C)
-#define LCD_ENTRY_DEC   							(0x04)
-#define LCD_ENTRY_DEC_SHIFT   						(0x05)
-#define LCD_ENTRY_INC_   							(0x06)
-#define LCD_ENTRY_INC_SHIFT   						(0x07)
-#define LCD_BEGIN_AT_FIRST_ROW						(0x80)
-#define LCD_BEGIN_AT_SECOND_ROW						(0xC0)
-#define LCD_CLEAR_SCREEN							(0x01)
-#define LCD_ENTRY_MODE								(0x06)
+#define LCD_CMD_FUNCTION_8BIT_2LINES   					(0x38)
+#define LCD_CMD_FUNCTION_4BIT_2LINES   					(0x28)
+#define LCD_CMD_MOVE_DISP_RIGHT       					(0x1C)
+#define LCD_CMD_MOVE_DISP_LEFT   						(0x18)
+#define LCD_CMD_MOVE_CURSOR_RIGHT   					(0x14)
+#define LCD_CMD_MOVE_CURSOR_LEFT 	  					(0x10)
+#define LCD_CMD_DISP_OFF   								(0x08)
+#define LCD_CMD_DISP_ON_CURSOR   						(0x0E)
+#define LCD_CMD_DISP_ON_CURSOR_BLINK   					(0x0F)
+#define LCD_CMD_DISP_ON_BLINK   						(0x0D)
+#define LCD_CMD_DISP_ON   								(0x0C)
+#define LCD_CMD_ENTRY_DEC   							(0x04)
+#define LCD_CMD_ENTRY_DEC_SHIFT   						(0x05)
+#define LCD_CMD_ENTRY_INC_   							(0x06)
+#define LCD_CMD_ENTRY_INC_SHIFT   						(0x07)
+#define LCD_CMD_BEGIN_AT_FIRST_ROW						(0x80)
+#define LCD_CMD_BEGIN_AT_SECOND_ROW						(0xC0)
+#define LCD_CMD_CLEAR_SCREEN							(0x01)
+#define LCD_CMD_ENTRY_MODE								(0x06)
 
 void LCD_INIT();
 void LCD_WRITE_COMMAND(unsigned char command);
 void LCD_WRITE_CHAR(unsigned char character);
 void LCD_WRITE_STRING(char* string);
 void LCD_ISBUSY(void);
-void LCD_clear_screen(void);
+void LCD_CLEAR_SCREEN(void);
+void LCD_GOTOXY(unsigned char line, unsigned char position);
+
 #endif /* LCD_H_ */
